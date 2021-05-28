b478c0244a849855bfa8ed9acd53a0f1de752909
JJRYY <kjr2436@gmail.com>
2021-05-28 09:43:33 +0900

    spring5_rest_study 초기설정

---
+++ src/main/java/spring5_rest_study/config/ContextRoot.java
@@ -0,0 +1,14 @@
+package spring5_rest_study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ContextDataSource.class, ContextSqlSession.class, MvcConfig.class})
+@ComponentScan(basePackages = {
+		 "spring5_webmvc_mybatis_study.mapper",  "spring5_webmvc_mybatis_study.controller"
+		 , "spring5_webmvc_mybatis_study.service", "spring5_webmvc_mybatis_study.common"  })
+public class ContextRoot {
+
+}
---
+++ src/main/java/spring5_rest_study/service/MemberService.java
@@ -0,0 +1,16 @@
+package spring5_rest_study.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import spring5_rest_study.dto.Member;
+
+@Service
+public interface MemberService {
+	List<Member> getLists();
+	Member getMember(long id);
+	int registerMember(Member member);
+	int modifyMember(Member member);
+	int removeMember(long id);
+}
